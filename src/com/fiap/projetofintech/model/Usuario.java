bab69a66551ddea429a41dff095932b335fede52
RaphaelSouzaFranco <raphchio@gmail.com>
2025-03-14 19:59:37 -0300

    retirada do comment

--- src/com/fiap/projetofintech/model/Usuario.java
+++ src/com/fiap/projetofintech/model/Usuario.java
@@ -8,7 +8,7 @@ public class Usuario {
     public Usuario(String nome, String email, String senha) {
         this.nome = nome;
         this.email = email;
-        this.senha = senha; 
+        this.senha = senha;
     }
 
     public String getNome() {
