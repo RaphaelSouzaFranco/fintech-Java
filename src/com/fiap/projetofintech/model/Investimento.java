41c6f710d883bdad55152169a5083f46a356289e
RaphaelSouzaFranco <raphchio@gmail.com>
2025-03-14 19:55:28 -0300

    implementação de atributo privado- classe Investimento

--- src/com/fiap/projetofintech/model/Investimento.java
+++ src/com/fiap/projetofintech/model/Investimento.java
@@ -2,12 +2,12 @@ package com.fiap.projetofintech.model;
 import java.time.LocalDateTime;
 
 public class Investimento {
-    String tipo;
-    String descricao;
-    LocalDateTime data; //Data com horário.
-    double valorInvestido;
-    String vencimento;
-    double rendimento;
+    private String tipo;
+    private String descricao;
+    private LocalDateTime data;
+    private double valorInvestido;
+    private String vencimento;
+    private double rendimento;
 
     public Investimento(String tipo, String descricao, LocalDateTime data, double valorInvestido, String vencimento, double rendimento){
         this.tipo = tipo;
