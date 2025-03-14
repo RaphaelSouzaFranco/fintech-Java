f65324d4829a4776fef12455b23206068816f8e8
RaphaelSouzaFranco <raphchio@gmail.com>
2025-03-14 19:38:49 -0300

    getters and setters Meta Cliente

--- src/com/fiap/projetofintech/model/MetaCliente.java
+++ src/com/fiap/projetofintech/model/MetaCliente.java
@@ -10,6 +10,31 @@ public class MetaCliente {
         this.valorMeta = valorMeta;
         this.prazoMeses = prazoMeses;
     }
+
+    public String getDefinirMeta() {
+        return definirMeta;
+    }
+
+    public void setDefinirMeta(String definirMeta) {
+        this.definirMeta = definirMeta;
+    }
+
+    public double getValorMeta() {
+        return valorMeta;
+    }
+
+    public void setValorMeta(double valorMeta) {
+        this.valorMeta = valorMeta;
+    }
+
+    public int getPrazoMeses() {
+        return prazoMeses;
+    }
+
+    public void setPrazoMeses(int prazoMeses) {
+        this.prazoMeses = prazoMeses;
+    }
+
     public void exibirMeta(){
         System.out.println("Meta: " + definirMeta + ", Valor Total: " + valorMeta + ",Prazo Meses: " + prazoMeses);
     }
