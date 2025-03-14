1747d16c60c6155ec5e2e49b4c2c688de3b97896
RaphaelSouzaFranco <raphchio@gmail.com>
2025-03-14 19:39:17 -0300

    getters and setters Cartao

--- src/com/fiap/projetofintech/model/Cartao.java
+++ src/com/fiap/projetofintech/model/Cartao.java
@@ -17,6 +17,46 @@ public class Cartao {
         this.cvc = cvc;
     }
 
+    public int getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(int cvc) {
+        this.cvc = cvc;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getNumero() {
+        return numero;
+    }
+
+    public void setNumero(double numero) {
+        this.numero = numero;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+
+    public void setBandeira(String bandeira) {
+        this.bandeira = bandeira;
+    }
+
     public void inserirCartao (){
         System.out.println("Digite a bandeira do cartão:");
         System.out.println("Digite o numero do cartão:");
