f878a7a0f62875634c8bfddc5e8eac406896323c
RaphaelSouzaFranco <raphchio@gmail.com>
2025-03-17 21:15:13 -0300

    nova classe

---
+++ src/com/fiap/projetofintech/model/PgAjuda.java
@@ -0,0 +1,54 @@
+package com.fiap.projetofintech.model;
+
+public class PgAjuda {private String esqueciSenha;
+    private String attCartao;
+    private String configurarInvestimento;
+    private String problemaPagamentos;
+    private String suporte;
+
+    // Getter e Setter para esqueciSenha
+    public String getEsqueciSenha() {
+        return esqueciSenha;
+    }
+
+    public void setEsqueciSenha(String esqueciSenha) {
+        this.esqueciSenha = esqueciSenha;
+    }
+
+    // Getter e Setter para attCartao
+    public String getAttCartao() {
+        return attCartao;
+    }
+
+    public void setAttCartao(String attCartao) {
+        this.attCartao = attCartao;
+    }
+
+    // Getter e Setter para configurarInvestimento
+    public String getConfigurarInvestimento() {
+        return configurarInvestimento;
+    }
+
+    public void setConfigurarInvestimento(String configurarInvestimento) {
+        this.configurarInvestimento = configurarInvestimento;
+    }
+
+    // Getter e Setter para problemaPagamentos
+    public String getProblemaPagamentos() {
+        return problemaPagamentos;
+    }
+
+    public void setProblemaPagamentos(String problemaPagamentos) {
+        this.problemaPagamentos = problemaPagamentos;
+    }
+
+    // Getter e Setter para suporte
+    public String getSuporte() {
+        return suporte;
+    }
+
+    public void setSuporte(String suporte) {
+        this.suporte = suporte;
+    }
+}
+
